2f198560b462f96315a8b42755e2a2f2710aa66d
Md Sazzad Anam Samin <22235103429@cse.bubt.edu.bd>
2025-10-29 18:50:36 +0600

    V3

---
+++ test/widget_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:studify_app/widgets/custom_button.dart';
+import 'package:studify_app/widgets/custom_text_field.dart';
+import 'package:studify_app/main.dart';
+
+void main() {
+  testWidgets('LoginScreen renders correctly', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(UniversityConnectApp());
+
+    // Verify that the Login screen shows the correct widgets.
+
+    // Checks for the "Login" title
+    expect(find.text('Login'), findsOneWidget);
+
+    // Checks for the email field
+    expect(find.widgetWithText(CustomTextField, 'University Email'), findsOneWidget);
+
+    // Checks for the password field
+    expect(find.widgetWithText(CustomTextField, 'Password'), findsOneWidget);
+
+    // Checks for the "Login" button
+    expect(find.widgetWithText(CustomButton, 'Login'), findsOneWidget);
+  });
+}
